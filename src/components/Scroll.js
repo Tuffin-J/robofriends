1392ab96ce2e6cc5480905f9f9b20ea5e464f79c
Josh <65804148+Tuffin-J@users.noreply.github.com>
2020-06-25 15:31:53 +1000

    Update package.json

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const Scroll = (props) => {
-	console.log(props);
 	return (
 		<div style={{overflowY: 'scroll', height: '27.5rem'}}>
 			{props.children}
 		</div>
 		);
 };
-// empty
 export default Scroll;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry.js';
 import './App.css';
 
 class App extends Component {
@@ -39,7 +40,9 @@ class App extends Component {
 			<h1 className='f1'>Robot Friends</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
 			<Scroll>
+			<ErrorBoundry>
 			<CardList robots={filteredRobots}/>
+			</ErrorBoundry>
 			</Scroll>
 			</div>
 		);
